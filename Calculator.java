b74fcc189f2705ead4dc167f067de249240ddd7a
brunosarm70 <brunosarm70@gmail.com>
2014-03-23 23:15:26 -0600

    Erase useless methods

--- Calculator.java
+++ Calculator.java
@@ -86,25 +86,4 @@ public class Calculator {
         }
     }
     
-    public int sum(int x,int y)
-    {
-        return (x+y);
-    }
-    public int subtraction(int x,int y)
-    {
-        return (x-y);
-    }
-    public int division(int x,int y)
-    {
-        return (x/y);
-    }
-    public int multiplication(int x,int y)
-    {
-        return (x*y);
-    }
-    public int pow(int x,int y)
-    {
-        return (x*y);
-    }
-    
 }
